8400c593cfc8b14dcd498d83f636f0c78bd59a59
Daniel Madzierski <daniel.madzierski.pl@gmai.com>
2026-06-08 18:39:39 +0200

    improve data model

--- receipt-analyzer-service/src/main/kotlin/pl/madzierski/daniel/app/receipt/ReceiptRepository.kt
+++ receipt-analyzer-service/src/main/kotlin/pl/madzierski/daniel/app/receipt/ReceiptRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import pl.madzierski.daniel.app.receipt.model.GetReceiptListItemResponse
+import pl.madzierski.daniel.app.receipt.model.GetReceiptListResponse
 
 @Repository
 interface ReceiptRepository : JpaRepository<ReceiptEntity, String> {
@@ -13,7 +13,7 @@ interface ReceiptRepository : JpaRepository<ReceiptEntity, String> {
         "SELECT id, name, description, created_date FROM receipt WHERE user_sub = :userSub ORDER BY created_date DESC",
         nativeQuery = true
     )
-    fun getReceiptList(@Param("userSub") userSub: String): List<GetReceiptListItemResponse>
+    fun getReceiptList(@Param("userSub") userSub: String): List<GetReceiptListResponse.GetReceiptListItemResponse>
 
     fun findReceiptEntityById(id: String): ReceiptEntity
 
--- receipt-analyzer-service/src/main/kotlin/pl/madzierski/daniel/app/receipt/model/GetReceiptListResponse.kt
+++ receipt-analyzer-service/src/main/kotlin/pl/madzierski/daniel/app/receipt/model/GetReceiptListResponse.kt
@@ -4,11 +4,11 @@ import java.util.*
 
 data class GetReceiptListResponse(
     val items: List<GetReceiptListItemResponse>
-)
-
-data class GetReceiptListItemResponse(
-    val id: String,
-    val name: String?,
-    val description: String?,
-    val createDate: Date?,
-)
+){
+    data class GetReceiptListItemResponse(
+        val id: String,
+        val name: String?,
+        val description: String?,
+        val createDate: Date?,
+    )
+}
